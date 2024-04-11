66bd512796015e45c8ce9b5be579fd70e15d7b7b
Luis Z <lezv.dc@gmail.com>
2024-04-11 13:32:53 -0600

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-import { MAIN_PATH } from './src/config'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/${MAIN_PATH}`
+  base: '/weather/'
 })
