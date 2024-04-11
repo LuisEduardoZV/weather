5035babc42d76cbabfa58315b7cdb7429e152140
Luis Z <lezv.dc@gmail.com>
2024-04-11 13:13:17 -0600

    Corrección error en github pages

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const MAIN_PATH = '/weather'
+export const MAIN_PATH = 'weather'
 
 export const BASE_URL_API = import.meta.env.VITE_BASE_URL_API
 export const KEY_API = import.meta.env.VITE_KEY_API
